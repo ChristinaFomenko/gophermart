4aea593ae5e73472d81e2f6d8638eb8633ef119a
Christina Fomenko <kris.inferno@gmail.com>
2022-08-16 16:46:35 +0300

    err.Is

--- internal/controller/http/handlers/accruals.go
+++ internal/controller/http/handlers/accruals.go
@@ -36,25 +36,30 @@ func (h *Handler) loadOrders(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	statusCode := http.StatusAccepted
+
 	err = h.Service.Accrual.LoadOrder(r.Context(), numOrder, userID)
 
 	if err != nil {
 		if !errors.Is(err, errs.ErrOrderAlreadyUploadedCurrentUser) {
-			http.Error(w, err.Error(), http.StatusOK)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		if !errors.Is(err, errs.ErrOrderAlreadyUploadedAnotherUser) {
-			http.Error(w, err.Error(), http.StatusConflict)
-			return
-		}
-		if !errors.Is(err, errs.ErrInvalidOrderNumberForm) {
-			http.Error(w, err.Error(), http.StatusUnprocessableEntity)
-			return
-		}
+		statusCode = http.StatusOK
 
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		if !errors.Is(err, errs.ErrOrderAlreadyUploadedAnotherUser) {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		statusCode = http.StatusConflict
+		if !errors.Is(err, errs.ErrInvalidOrderNumberForm) {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		statusCode = http.StatusUnprocessableEntity
+
 	}
+
 	//switch err.(type) {
 	//case nil:
 	//	w.WriteHeader(http.StatusAccepted)
@@ -70,7 +75,7 @@ func (h *Handler) loadOrders(w http.ResponseWriter, r *http.Request) {
 	//default:
 	//	http.Error(w, errs.InternalServerError, http.StatusInternalServerError)
 	//}
-	w.WriteHeader(http.StatusAccepted)
+	w.WriteHeader(statusCode)
 }
 
 func (h *Handler) getUploadedOrders(w http.ResponseWriter, r *http.Request) {
