f6705eceb06a637aa5ed3a31b93ba432589dbbac
Christina Fomenko <kris.inferno@gmail.com>
2022-08-18 15:14:56 +0300

    user additions

--- internal/repository/psql/psql.go
+++ internal/repository/psql/psql.go
@@ -38,22 +38,25 @@ func (p *Psql) BeginTx(ctx context.Context) (*sql.Tx, error) {
 }
 
 func (p *Psql) Init() error {
-	_, err := p.DB.Exec(`CREATE TABLE IF NOT EXISTS public.users(
+	_, err := p.DB.Exec(`CREATE TABLE IF NOT EXISTS users(
 		    id SERIAL PRIMARY KEY,
     		login TEXT NOT NULL UNIQUE,
-    		password TEXT NOT NULL);
+    		password TEXT NOT NULL,
+    		"current" FLOAT NOT NULL DEFAULT 0,
+        	withdrawal FLOAT NOT NULL DEFAULT 0
+    		);
 
-			CREATE TABLE IF NOT EXISTS public.orders(
+			CREATE TABLE IF NOT EXISTS orders(
 				id BIGSERIAL PRIMARY KEY,
 				order_num BIGINT UNIQUE,
 				user_id INT NOT NULL,
 				FOREIGN KEY (user_id) REFERENCES public.users (id));
 
-			CREATE TABLE IF NOT EXISTS public.accruals(
+			CREATE TABLE IF NOT EXISTS accruals(
 				order_num BIGINT PRIMARY KEY,
 				user_id INT NOT NULL,
 				status TEXT NOT NULL DEFAULT 'NEW',
-				amount REAL DEFAULT 0,
+				amount FLOAT DEFAULT 0,
 				uploaded_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
 				FOREIGN KEY (user_id) REFERENCES public.users (id),
 			    FOREIGN KEY (order_num) REFERENCES public.orders (order_num));
@@ -61,7 +64,7 @@ func (p *Psql) Init() error {
 			CREATE TABLE IF NOT EXISTS withdrawals(
 			    order_num BIGINT PRIMARY KEY,
 				user_id INT NOT NULL,
-				amount REAL DEFAULT 0,
+				amount FLOAT DEFAULT 0,
 				processed_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
 				FOREIGN KEY (user_id) REFERENCES public.users (id),
 			    FOREIGN KEY (order_num) REFERENCES public.orders (order_num));
